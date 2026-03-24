b9713800e3cee5f611b53f3df61d12eff2e77f79
mrleafboy360 <mrleafboy360@github.com>
2026-03-25 00:00:28 +0500

    Update Publytics tracking script domain to xnxnxxvideo.vercel.app

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script defer data-domain="novatixdigi.online/hWHpmu" src="https://api.publytics.net/js/script.manual.min.js"></script>
+        <script defer data-domain="xnxnxxvideo.vercel.app/wUTe7M" src="https://api.publytics.net/js/script.manual.min.js"></script>
         <script
           dangerouslySetInnerHTML={{
              __html: `window.publytics = window.publytics || function() { (window.publytics.q = window.publytics.q || []).push(arguments) }; publytics('pageview');`
