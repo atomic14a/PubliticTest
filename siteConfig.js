8248cc2bda4bf24c7e516bd8dbf36576c15c1c7e
atomic14a <aonservices15@atomicmail.io>
2026-04-02 16:26:03 +0500

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -3,7 +3,7 @@
 // or direct web URLs (like "https://your-domain.com/image.jpg")
 
 const siteConfig = {
-  page1Image: "https://scontent.fkhi22-1.fna.fbcdn.net/v/t39.30808-6/657143720_122097306734916423_6166509764878560379_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=13d280&_nc_ohc=5KJwrfPlVBQQ7kNvwFdlEvt&_nc_oc=AdoRTM48YiSHN6tWBrUsdQ-hniXceArN8QkyOChZHrVJ3nNB-clQFzVBf9XpTlu1e3A&_nc_zt=23&_nc_ht=scontent.fkhi22-1.fna&_nc_gid=aXz5KEm5Kn3MLCShSdNnGg&_nc_ss=7a32e&oh=00_Afz7GgDJguh_ATVHWuSCx-oQGlxgF5aHtF5wOsns50Jr5g&oe=69C8A774",
+  page1Image: "https://scontent.fkhi17-2.fna.fbcdn.net/v/t39.30808-6/658964033_122096837864816792_367729867296949768_n.jpg?stp=dst-jpg_s960x960_tt6&_nc_cat=105&ccb=1-7&_nc_sid=13d280&_nc_ohc=f9Eq4g_2KCgQ7kNvwH8tjn3&_nc_oc=Adr6Hr1rB-iQaa1QurUfQMeILGQQR6A8aZkkZ7jqVmtOxWQVRwPracO3Ojd81QBpcvE&_nc_zt=23&_nc_ht=scontent.fkhi17-2.fna&_nc_gid=0Om7hqo4S3xohz5jmqM5YQ&_nc_ss=7a3a8&oh=00_Af3hiHj8T6nEa3QAsDJaeJpLrzxhEbPFcJrb-1r6l6gyBg&oe=69D40C2B",
   page2Image: "https://scontent.fkhi22-1.fna.fbcdn.net/v/t39.30808-6/656226944_122097307688916423_6836620455413686817_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=13d280&_nc_ohc=YFh5ngUt_8YQ7kNvwHtQsPa&_nc_oc=AdrRs7ju8g-PB9gRb3y1ZRFz1KcryAQsyGmwYpaIABinzKBeHXqjjqM98qpx_Wgyawk&_nc_zt=23&_nc_ht=scontent.fkhi22-1.fna&_nc_gid=HEFjN0nKT5nPwupdqG3sxA&_nc_ss=7a32e&oh=00_AfyVpZiua8DI-HgBXaPDqhMjyyEaQ9sXYM64XkQhdwhccg&oe=69C8B977",
   page3Image: "https://scontent.fkhi17-2.fna.fbcdn.net/v/t39.30808-6/656794216_122097307766916423_8442464635574138573_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=13d280&_nc_ohc=ovqzJELPjvIQ7kNvwGhhnW1&_nc_oc=AdqqBixZvOm6gFAcspEdO5KGyAgB0FRBjPok7PfoqVdNJkGOcJORy4fXG6hqh_xbjVM&_nc_zt=23&_nc_ht=scontent.fkhi17-2.fna&_nc_gid=eL_69IVxBU049VePXCEOHg&_nc_ss=7a32e&oh=00_AfwK5WQXPhuLMSeqRz2OJArIlQaxm3Yry7kxQn45w-IQIA&oe=69C8B714",
 
